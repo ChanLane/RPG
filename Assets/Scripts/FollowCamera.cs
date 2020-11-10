04564afbb7af663682be4f876d60c197673afef5
Chandler <chandler.script@gmail.com>
2020-11-09 23:38:24 -0500

    camera follow

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChandlerLane.Scripts.FollowCamera
+{
+    
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _target;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            this.transform.position = _target.transform.position;
+        }
+    }
+
+}
