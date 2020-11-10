17322f6043e7f52e973c842b7211ae6513e05a91
Chandler <chandler.script@gmail.com>
2020-11-09 23:17:42 -0500

    click to move

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ChandlerLane.Scripts.Mover
+{
+    
+    
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _target;
+        private NavMeshAgent _agent;
+
+        private Ray _lastRay;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (_agent == null)
+            {
+                _agent = GetComponent<NavMeshAgent>();
+            }
+
+        }
+
+        void Update()
+        {
+            _agent.destination = _target.transform.position;
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                _lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
+            Debug.DrawRay(_lastRay.origin, _lastRay.direction * 100);    
+        }
+    }
+
+}
+
